55aa36b116e35d21b9795f9f9c0268e27ede6b56
luiz <luiz_tauffer@hotmail.com>
2022-11-18 17:07:46 +0100

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,76 @@
-# spikeinterface_cloud
+# Build and deploy the Docker images
+
+Build docker image:
+```bash
+$ DOCKER_BUILDKIT=1 docker build -t <image-name:version> -f <Dockerfile_name> .
+```
+
+Run locally:
+```bash
+# With mounted volume
+$ docker run \
+    -v <host_path>:/results \
+    --gpus all \
+    --env AWS_S3_BUCKET=${AWS_S3_BUCKET} \
+    --env AWS_S3_BUCKET_FOLDER=${AWS_S3_BUCKET_FOLDER}  \
+    --env AWS_REGION_NAME=${AWS_REGION_NAME}  \
+    --env AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}  \
+    --env AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}  \
+    <image-name:version>
+
+# Run with access to a bash terminal inside the running container
+$ docker run -it --gpus all --entrypoint /bin/bash <image-name:version>
+```
+
+Push image to ECR:
+```bash
+# Login to ECR
+$ aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin <aws_id>.dkr.ecr.us-east-1.amazonaws.com
+
+# Tag image to comply with ECR
+$ docker tag <image-name:version> <aws_id>.dkr.ecr.us-east-2.amazonaws.com/<image-name:version>
+
+# Push image
+$ docker push <aws_id>.dkr.ecr.us-east-2.amazonaws.com/<image-name:version>
+```
+
+
+# AWS Batch configuration
+
+1. Create a Compute environment (EC2)
+    - Use the standard IAMs: Service Role = AWSServiceRoleForBatch ([ref](https://docs.aws.amazon.com/batch/latest/userguide/service_IAM_role.html)) and Instance Role = ecsInstanceRole ([ref](https://docs.aws.amazon.com/batch/latest/userguide/instance_IAM_role.html))
+    - Configure the instances available to be used by the environment. Instances with access to GPU instances (e.g. g3s.xlarge) might require you to request AWS to increase the quota limit for such machines, which by default is 0.
+    - If you choose Spot instances, choose a value between 30~50% for the `Maximum % on-demand price`
+2. Create a Job Queue (EC2) associated with the compute environment
+3. Create a Job Definition (EC2)
+    - choose suitable Execution Timeout, Job Attempts and Retry Strategies, if suitable
+    - Select the base image
+    - Command = `python run_script.py`
+    - For the Job role configuration, choose an IAM role with permissions to read and write to S3 buckets and with Trusted entities configured like this:
+    ```
+    {
+    "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Effect": "Allow",
+                "Principal": {
+                    "Service": "ecs-tasks.amazonaws.com"
+                },
+                "Action": "sts:AssumeRole"
+            }
+        ]
+    }
+    ```
+    - Configure the hardware specs
+    - Add any fixed ENV variables that should be used by any Jobs using this definition
+
+
+# Submiting jobs
+
+The job submission must include the function arguments, which are passed and ENV vars to the running container.
+
+
+# Useful refs:
+- https://medium.com/@michael.smith.qs2/how-to-use-gpus-quickly-and-cheaply-with-aws-batch-and-pytorch-1209320c4e6b
+- https://github.com/michael-smith-qs2/aws_gpu_batch_setup_2021
+- https://github.com/NVIDIA/nvidia-container-runtime#environment-variables-oci-spec
---
+++ src/run_script.py
@@ -0,0 +1,127 @@
+import boto3
+import botocore
+import os
+from pathlib import Path
+import spikeinterface.extractors as se
+from spikeinterface.sorters import run_sorter_local
+import spikeinterface.comparison as sc
+
+
+def download_all_files_from_bucket_folder(
+    client:botocore.client.BaseClient, 
+    bucket_name:str, 
+    bucket_folder:str
+):    
+    # List files in folder, download all files with content
+    res = client.list_objects_v2(Bucket=bucket_name, Prefix=bucket_folder)
+    for f in res["Contents"]:
+        if f["Size"] > 0:
+            file_name = f["Key"].split("/")[-1]
+            client.download_file(
+                Bucket=bucket_name, 
+                Key=f["Key"], 
+                Filename=f"data/{file_name}"
+            )
+
+
+def upload_all_files_to_bucket_folder(
+    client:botocore.client.BaseClient, 
+    bucket_name:str, 
+    bucket_folder:str,
+    local_folder:str
+):
+    # List files from results, upload them to S3
+    files_list = [f for f in Path(local_folder).rglob("*") if f.is_file()]
+    for f in files_list:
+        print(f"Uploading {str(f)}...")
+        client.upload_file(
+            Filename=str(f),
+            Bucket=bucket_name,
+            Key=f"{bucket_folder}{str(f)}",
+        )
+
+
+if __name__ == '__main__':
+    # S3 client
+    client = boto3.client(
+        's3', 
+        # region_name=os.environ["AWS_REGION_NAME"], 
+        # aws_access_key_id=os.environ["AWS_ACCESS_KEY_ID"],
+        # aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"]
+    )
+
+    # Run-specific arguments are retireved from ENV vars
+    bucket_name = os.environ["AWS_S3_BUCKET"]
+    bucket_folder = os.environ["AWS_S3_BUCKET_FOLDER"]
+
+    # Download data
+    print(f"Downloading dataset: {os.environ['AWS_S3_BUCKET']}/{os.environ['AWS_S3_BUCKET_FOLDER']}")
+    download_all_files_from_bucket_folder(
+        client=client,
+        bucket_name=bucket_name, 
+        bucket_folder=bucket_folder
+    )
+
+    # Read SpikeGLX data into recording
+    print("Reading recording...")
+    recording = se.read_spikeglx(folder_path="/data", stream_id="imec.ap")
+
+
+    # recording, _ = se.toy_example(
+    #     duration=10,
+    #     seed=0,
+    #     num_channels=64,
+    #     num_segments=1
+    # )
+
+    # Run sorter
+    print("Running Kilosort 3...")
+    output_folder = '/results/sorting'
+    sorting_ks3 = run_sorter_local(
+        'kilosort3', 
+        recording, 
+        output_folder=output_folder,
+        remove_existing_folder=True, 
+        delete_output_folder=True,
+        verbose=True, 
+        raise_error=True, 
+        with_output=True
+    )
+    sorting_ks3.save_to_folder(folder='/results/sorter_exported_ks3')
+
+    print("Running Kilosort 2.5...")
+    output_folder = '/results/sorting'
+    sorting_ks25 = run_sorter_local(
+        'kilosort2_5', 
+        recording, 
+        output_folder=output_folder,
+        remove_existing_folder=True, 
+        delete_output_folder=True,
+        verbose=True, 
+        raise_error=True, 
+        with_output=True
+    )
+    sorting_ks25.save_to_folder(folder='/results/sorter_exported_ks25')
+
+    print("Running sorters comparison...")
+    mcmp = sc.compare_multiple_sorters(
+        sorting_list=[sorting_ks3, sorting_ks25],
+        name_list=['KS3', 'KS25'],
+        verbose=True,
+    )
+    print("Matching results:")
+    print(mcmp.comparisons[('KS3', 'KS25')].get_matching())
+
+    # Upload sorting results to S3
+    upload_all_files_to_bucket_folder(
+        client=client, 
+        bucket_name=bucket_name, 
+        bucket_folder=bucket_folder,
+        local_folder="results/sorter_exported_ks3"
+    )
+    upload_all_files_to_bucket_folder(
+        client=client, 
+        bucket_name=bucket_name, 
+        bucket_folder=bucket_folder,
+        local_folder="results/sorter_exported_ks25"
+    )
