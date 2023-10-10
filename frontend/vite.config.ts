9930f0ce463db90f41fcb15a057462b8302830fb
luiz <luiz_tauffer@hotmail.com>
2023-10-10 14:31:22 +0200

    fix for dockr compose

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true, // needed for the Docker Container port mapping to work
+    strictPort: true,
+    port: 5173, // replace this port with any number you want
+  },
 })
